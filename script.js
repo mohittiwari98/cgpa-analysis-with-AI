332a5430cffee47e8f4dd3c8a3c29c1b0d86c186
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-07-01 18:34:41 +0530

    script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const studentsData = [
 ];
 //Use your own api key
 const API_KEY = "YOUR API";
-const API_URL = "API ";
+const API_URL = "API U";
 
 let chartInstance = null;
 let currentTab = 'sgpa';
